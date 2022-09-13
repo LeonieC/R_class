1f88f7b569487e8b238f9379aa2bc990d159e5e9
LeonieC <lisa89731@gmail.com>
2022-09-14 00:47:41 +0800

    last for 0913

--- try.R
+++ try.R
@@ -11,3 +11,5 @@ fish # print my object `fish`
 fish2<-read.table('D:/R_wd/week1/R_classs/R_classs/reef_fish.txt', header=T, sep='\t', dec='.') 
 
 barplot(fish2$richness, main="Top 10 reef fish Richness (Allen, 2000)", horiz=TRUE, names.arg=fish$country, cex.names=0.5, las=1)
+
+###
