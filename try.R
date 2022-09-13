89d074961525ff7a911380d8a5dc67b2917240ac
LeonieC <lisa89731@gmail.com>
2022-09-14 00:18:34 +0800

    last for 9/13

--- try.R
+++ try.R
@@ -1,1 +1,13 @@
-#just for try
+fish<-read.table('D:/R_wd/week1/R_classs/R_classs/reef_fish.txt', header=T, sep='\t', dec='.')
+barplot(fish$richness, main="Top 10 reef fish Richness (Allen, 2000)", horiz=TRUE, names.arg=fish$country, cex.names=0.5, las=1)
+
+library(readxl) # load the package `readxl'
+read_excel('D:/R_wd/week1/R_classs/R_classs/reef_fish.xlsx') # automatically print on my screen
+
+
+fish<-read_excel('D:/R_wd/week1/R_classs/R_classs/reef_fish.xlsx') # store my table in an object called `fish`
+fish # print my object `fish`   
+
+fish2<-read.table('D:/R_wd/week1/R_classs/R_classs/reef_fish.txt', header=T, sep='\t', dec='.') 
+
+barplot(fish2$richness, main="Top 10 reef fish Richness (Allen, 2000)", horiz=TRUE, names.arg=fish$country, cex.names=0.5, las=1)
